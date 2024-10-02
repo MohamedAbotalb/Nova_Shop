6b49abda6592412cee0f01514dd6bf38460f4c00
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-10-02 18:32:13 +0300

    Add spring boot starter project files

---
+++ nova-shop-api/src/main/java/com/mabotalb/nova_shop_api/NovaShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.mabotalb.nova_shop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaShopApiApplication.class, args);
+	}
+
+}
---
+++ nova-shop-api/src/test/java/com/mabotalb/nova_shop_api/NovaShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabotalb.nova_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
