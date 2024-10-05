62bc20fba3eefc7222cef9f3c55793b7145d1a73
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-10-05 12:19:23 +0300

    (feat) Add BaseEntity class to use as a super class with different entities

---
+++ nova-shop-api/src/main/java/com/mabotalb/nova_shop_api/common/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.mabotalb.nova_shop_api.common;
+
+import jakarta.persistence.*;
+import lombok.experimental.SuperBuilder;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Long lastModifiedBy;
+}
