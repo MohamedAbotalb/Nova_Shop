b3486d5fd8b9c74a1a53ddb6e7d34f6903513753
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-10-06 00:35:56 +0300

    (feat) Add category service to manage the logic for crud operations for category entity

---
+++ nova-shop-api/src/main/java/com/mabotalb/nova_shop_api/category/ICategoryService.java
@@ -0,0 +1,17 @@
+package com.mabotalb.nova_shop_api.category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    List<CategoryDto> getAllCategories();
+
+    CategoryDto getCategoryById(Long id);
+
+    CategoryDto getCategoryByName(String name);
+
+    CategoryDto addCategory(CategoryRequest request);
+
+    CategoryDto updateCategory(CategoryRequest request, Long id);
+
+    void deleteCategoryById(Long id);
+}
