ad4ff22c29e1b691bec2a2939352f8a5af24b380
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-10-05 12:45:13 +0300

    (feat) Add product repository

---
+++ nova-shop-api/src/main/java/com/mabotalb/nova_shop_api/product/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.mabotalb.nova_shop_api.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryName(String category);
+
+    List<Product> findByBrand(String brand);
+
+    List<Product> findByCategoryNameAndBrand(String category, String brand);
+
+    List<Product> findByName(String name);
+
+    List<Product> findByBrandAndName(String brand, String name);
+
+    Integer countByBrandAndName(String brand, String name);
+}
